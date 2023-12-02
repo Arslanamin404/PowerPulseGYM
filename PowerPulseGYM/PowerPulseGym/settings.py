670824b9e75c2634f30e756102e94f740806b8c0
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2023-12-02 12:46:43 +0530

    Removed unnecessary files

--- PowerPulseGYM/PowerPulseGym/settings.py
+++ PowerPulseGYM/PowerPulseGym/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u$1l7mytv!tauykbxj)40yi8e-ou32=)+czwmde06zdhtptc8r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # false for deployment and True for development
+DEBUG = False #false for deployment and True for development
 
 ALLOWED_HOSTS = ['*']
 
@@ -131,10 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
